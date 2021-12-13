c7ddbbff2c823fd4b0e161809a1f07d69ef3d48d
Michal Pol <dissected9@gmail.com>
2021-12-13 22:54:48 +0100

    23.3 Change timer

--- src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -16,8 +16,8 @@ public class EmailScheduler {
     private final SimpleEmailService simpleEmailService;
     private final TaskRepository taskRepository;
     private final AdminConfig adminConfig;
-
-    @Scheduled(fixedDelay = 10000)
+    
+    @Scheduled(cron = "0 0 10 * * *")
     public void sendInformationEmail() {
         long size = taskRepository.count();
         simpleEmailService.send(
